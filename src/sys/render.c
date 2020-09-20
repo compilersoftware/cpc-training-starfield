006784d6330dd972a8575b3567427d76729258f1
Federico J. Álvarez Valero <fede.alvarez@gmail.com>
2020-09-20 21:48:17 +0200

    Implement colours based on speed

--- src/sys/render.c
+++ src/sys/render.c
@@ -5,10 +5,10 @@
 
 const u8 _palette[] = {
     HW_BLACK,
-    HW_BRIGHT_WHITE,
-    HW_BRIGHT_WHITE,
-    HW_BRIGHT_WHITE,
-    HW_BRIGHT_WHITE,
+    HW_BLUE,
+    HW_SKY_BLUE,
+    HW_BRIGHT_BLUE,
+    HW_BRIGHT_CYAN,
     HW_BRIGHT_WHITE,
     HW_BRIGHT_WHITE,
     HW_BRIGHT_WHITE,
@@ -24,6 +24,30 @@ const u8 _palette[] = {
 
 /* Funciones privadas */
 
+/**
+ * @precondition speed tiene signo, pero ha de tener un valor absoluto entre 1 y 4
+ */
+u8 _sys_render_getColourFromSpeed(i8 speed)
+{
+    if (speed < 0) {
+        speed = speed * -1;
+    }
+    switch (speed) {
+        case 1:
+            return 0x80;
+        case 2:
+            return 0x08;
+        case 3:
+            return 0x88;
+        case 4:
+            return 0x20;
+        default:
+            // No debería ocurrir, lo ponemos para evitar el warning del compilador
+            return 0x00;
+    }
+}
+
+
 void _sys_render_updateSingleEntity(Entity_t* entity)
 {
     // Borra la anterior (si existe)
@@ -35,8 +59,9 @@ void _sys_render_updateSingleEntity(Entity_t* entity)
     // Draw current (if it is not marked for destruction)
     if (!(entity->type & entityTypeDead)) {
         u8* pVideoMem = cpct_getScreenPtr(CPCT_VMEM_START, entity->x, entity->y);
-        // Escribe entity->colour en la celda de memoria a la que apunta pVideoMem
-        *pVideoMem = entity->colour; 
+        u8 colour = _sys_render_getColourFromSpeed(entity->vx);
+        // Escribe colour en la celda de memoria a la que apunta pVideoMem
+        *pVideoMem = colour; 
         entity->prevPtr = pVideoMem;
     }
 }
